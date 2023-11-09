4541ed1d6b5e90e776e2ef106b106b612df74540
Jens <69081683+jenslys@users.noreply.github.com>
2023-11-09 16:05:58 +0100

    make the orders-table collapsed

--- src/components/OrdersTable.js
+++ src/components/OrdersTable.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import "firebase/firestore";
 import { db } from "../firebase/config";
 import { collection, getDocs, query, orderBy } from "firebase/firestore";
+import { Box, Collapse, IconButton, Typography } from "@mui/material";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 import {
   Table,
@@ -21,6 +24,7 @@ export default function OrdersTabel() {
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [openRowId, setOpenRowId] = useState(null);
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -44,6 +48,10 @@ export default function OrdersTabel() {
     setPage(0);
   };
 
+  const handleRowClick = (id) => {
+    setOpenRowId(openRowId === id ? null : id);
+  };
+
   return (
     <div style={{ padding: "16px" }}>
       <Paper style={{ padding: "16px" }}>
@@ -59,37 +67,64 @@ export default function OrdersTabel() {
               <CircularProgress />
             </div>
           ) : (
-            <Table size="small">
+            <Table size="small" dense>
               <TableHead>
                 <TableRow>
+                  <TableCell />
                   <TableCell>Full Name</TableCell>
                   <TableCell>Phone Number</TableCell>
                   <TableCell>Address</TableCell>
-                  <TableCell>Items Ordered</TableCell>
                   <TableCell>Total Price</TableCell>
                   <TableCell>Date</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
                 {orders.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((order) => (
-                  <TableRow key={order.id}>
-                    <TableCell>
-                      {order.firstName} {order.lastName}
-                    </TableCell>
-                    <TableCell>{order.phoneNumber}</TableCell>
-                    <TableCell>
-                      <Link
-                        href={`https://maps.google.com/?q=${order.address}, ${order.city}, ${order.zipCode}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        underline="hover">
-                        {order.address}, {order.city}, {order.zipCode}
-                      </Link>
-                    </TableCell>
-                    <TableCell>{order.itemsBought.join(", ")}</TableCell>
-                    <TableCell>{order.totalPrice} NOK</TableCell>
-                    <TableCell>{order.date.toDate().toLocaleString()}</TableCell>
-                  </TableRow>
+                  <React.Fragment key={order.id}>
+                    <TableRow onClick={() => handleRowClick(order.id)}>
+                      <TableCell>
+                        <IconButton size="small">
+                          {openRowId === order.id ? (
+                            <KeyboardArrowUpIcon />
+                          ) : (
+                            <KeyboardArrowDownIcon />
+                          )}
+                        </IconButton>
+                      </TableCell>
+                      <TableCell>
+                        {order.firstName} {order.lastName}
+                      </TableCell>
+                      <TableCell>{order.phoneNumber}</TableCell>
+                      <TableCell>
+                        <Link
+                          href={`https://maps.google.com/?q=${order.address}, ${order.city}, ${order.zipCode}`}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          underline="hover">
+                          {order.address}, {order.city}, {order.zipCode}
+                        </Link>
+                      </TableCell>
+                      <TableCell>{order.totalPrice} NOK</TableCell>
+                      <TableCell>{order.date.toDate().toLocaleString()}</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+                        <Collapse in={openRowId === order.id} timeout="auto" unmountOnExit>
+                          <Box sx={{ margin: 1 }}>
+                            <Typography variant="h6" gutterBottom component="div">
+                              Items Ordered
+                            </Typography>
+                            {order.itemsBought.map((item) => (
+                              <Typography key={item}>{item}</Typography>
+                            ))}
+                            <Typography variant="h6" gutterBottom component="div">
+                              Total Price: {order.totalPrice} NOK
+                            </Typography>
+                          </Box>
+                        </Collapse>
+                      </TableCell>
+                    </TableRow>
+                  </React.Fragment>
                 ))}
               </TableBody>
             </Table>
